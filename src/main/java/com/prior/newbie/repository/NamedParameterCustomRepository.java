573843c6c45e3f6688c61d3c7086a321843877c7
titima <satara_t@silpakorn.edu>
2022-05-10 16:48:30 +0700

    edit update people from NamedParameterJdbcTemplate

--- src/main/java/com/prior/newbie/repository/NamedParameterCustomRepository.java
+++ src/main/java/com/prior/newbie/repository/NamedParameterCustomRepository.java
@@ -2,16 +2,14 @@ package com.prior.newbie.repository;
 
 import com.prior.newbie.entities.People;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.util.ParameterMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -47,14 +45,20 @@ public class NamedParameterCustomRepository {
         String sql = "UPDATE PEOPLE " +
                      "SET TITLE = :title, FIRSTNAME = :firstname, LASTNAME = :lastname, MIDDLE_NAME = :middle_name, MOBILE = :mobile, GENDER = :gender, BIRTH_DATE = :birth_date, IS_DELETED = :is_deleted, CREATED_BY = :created_by, CREATED_DATE = :created_date, UPDATE_BY = :update_by, UPDATE_DATE = :update_date " +
                      "WHERE cid = :cid";
-        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource("cid", cid));
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource("cid", cid).addValue("title", people.getTitle())
+                .addValue("firstname", people.getFirstname()).addValue("lastname", people.getLastname())
+                .addValue("middle_name", people.getMiddle_name()).addValue("mobile", people.getMobile())
+                .addValue("gender", people.getGender()).addValue("birth_date", people.getBirth_date())
+                .addValue("is_deleted", people.getIs_deleted()).addValue("created_by",people.getCreated_by())
+                .addValue("created_date", people.getCreated_date()).addValue("update_by", people.getUpdate_by())
+                .addValue("update_date",people.getUpdate_date()));
         return "update succeed";
     }
 
     public String deletePeople(String cid) {
         System.out.println("######## delete people namedParameterJdbcTemplate ########");
         String sql = "UPDATE PEOPLE SET IS_DELETED = 'Y' WHERE cid = :cid";
-        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource("cid", cid));
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource( "cid", cid));
         return "delete succeed";
     }
 
